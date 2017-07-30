269a0c64bc1a3fd416ce5480300ecc99923975f0
Alex Visconde <andres.visconde@gmail.com>
2017-07-30 14:22:57 -0400

    profile-picture spec pass

--- spec/features/profile_photo_spec.rb
+++ spec/features/profile_photo_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 feature "profile photo" do
-  xscenario "user uploads a profile photo" do
+  scenario "user uploads a profile photo" do
     visit root_path
     click_link "Sign Up"
     fill_in "First Name", with: "Alex"
     fill_in "Last Name", with: "Vis"
     fill_in "Email", with: "ash@s-mart.com"
     attach_file "user_profile_photo", "#{Rails.root}/spec/support/images/photo.png"
-    click_button "Upload Picture"
     fill_in "Password", with: "password"
     fill_in "Password Confirmation", with: "password"
     click_button "Sign Up"
