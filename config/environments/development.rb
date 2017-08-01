dcdef414d81975121a590e0a836fba084c1b73e1
Alex Visconde <andres.visconde@gmail.com>
2017-08-01 09:50:26 -0400

    reset password divise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,24 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'helloteacherapp@gmail.com',
+  password:             'Cheto1950',
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
+  config.action_mailer.default_url_options = {
+    host: "localhost",
+    port: 3000
+  }
+
+
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -31,7 +49,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
