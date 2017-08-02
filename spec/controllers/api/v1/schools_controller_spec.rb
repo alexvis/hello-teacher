c886a7c8cf0208bf2732cf5fe5027c3cf8b92312
Alex Visconde <andres.visconde@gmail.com>
2017-08-02 19:21:59 -0400

    add spec for school api

--- spec/controllers/api/v1/schools_controller_spec.rb
+++ spec/controllers/api/v1/schools_controller_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Api::V1::SchoolsController, type: :controller do
     address: "Ciceron 6237",
     city: "Montevideo",
     state: "CA",
-    zip_code: 12200,
+    zip_code: "12200",
     ) }
 
-  xdescribe "GET#index" do
+  describe "GET#index" do
     it "should return details about a school" do
 
       get :index
@@ -17,13 +17,12 @@ RSpec.describe Api::V1::SchoolsController, type: :controller do
 
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
-
       expect(returned_json.length).to eq 1
-      expect(returned_json[0]["name"]).to eq "Yosamite"
-      expect(returned_json["address"]).to eq "Ciceron 6237"
-      expect(returned_json["city"]).to eq  "Montevideo"
-      expect(returned_json["state"]).to eq "CA"
-      expect(returned_json["zip_code"]).to eq  12200
+      expect(returned_json["schools"][0]["name"]).to eq "Yosamite"
+      expect(returned_json["schools"][0]["address"]).to eq "Ciceron 6237"
+      expect(returned_json["schools"][0]["city"]).to eq  "Montevideo"
+      expect(returned_json["schools"][0]["state"]).to eq "CA"
+      expect(returned_json["schools"][0]["zip_code"]).to eq  "12200"
     end
   end
 
