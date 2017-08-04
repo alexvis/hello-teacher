b5e4d278f0ac3deb9e6856d99f905b9a2bb1c4ff
Alex Visconde <andres.visconde@gmail.com>
2017-08-04 13:04:34 -0400

    routes classroom delete

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :classrooms
-    end
-  end
-  
-  namespace :api do
-    namespace :v1 do
       resources :users do
         resources :schools do
           resources :classrooms
