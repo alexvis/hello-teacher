a85180408759013dde250dd6d265d1039475c50a
Alex Visconde <andres.visconde@gmail.com>
2017-08-09 08:19:38 -0400

    change assest pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require rails-ujs
 //= require_tree .
 
-$(function(){ $(document)
+$(function(){ $(document).foundation(); });
